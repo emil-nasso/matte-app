447121ecd8a5e438a06f006081e5060e576f773a
Emil Andersson <emil@glesys.se>
2017-03-30 18:31:20 +0200

    Result randomization instead of equation randomization to make the equations a bit more spread out and easier

--- src/ui/components/matte-app/component.ts
+++ src/ui/components/matte-app/component.ts
@@ -26,38 +26,47 @@ export default class MatteApp extends Component {
 
   guess(event) {
     let guess = +event.target.value
-    console.log("guess:",guess)
-    console.log(event)
     if (guess == this.equation.result) {
-      console.log("correct!")
       this.score++
       this.newEquation()
       event.target.value = ""
-      this.newGif()
-    } else {
-      console.log("wrong.")
+      // this.newGif()
     }
   }
 
-  async newGif(){
+  // async newGif(){
     //let gifJson = $.getJSON(`http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC`)
     /*let result = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC`)
     console.log(result)
     let gifJson = await result.json()
     console.log(gifJson)
     console.log(gifJson)*/
-  }
+  // }
 
   newEquation(){
-    let left = this.randomIntFromInterval(1, 9)
-    let right = this.randomIntFromInterval(1, 9)
+    // let left = this.randomIntFromInterval(1, 9)
+    // let right = this.randomIntFromInterval(1, 9)
 
-    this.equation = {
-      left,
-      sign: "+",
-      right,
-      result: left + right
+    var result = this.randomIntFromInterval(2,10)
+    console.log(result)
+    let correctEquations = []
+    for (var left = 1; left<10; left++) {
+      for (let right = 1;right<10; right++) {
+        if ((left + right) == result) {
+          correctEquations.push({
+            left,
+            right,
+            result,
+            sign: "+"
+          })
+        }
+      }
     }
+
+    let index = this.randomIntFromInterval(0, correctEquations.length-1)
+    let equation = correctEquations[index]
+    console.log(correctEquations, index, equation )
+    this.equation = equation
   }
 
   randomIntFromInterval(min,max)
